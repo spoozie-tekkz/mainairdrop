58475e34c4ae9edfa9b65a6fb8ad4e9996170c68
spoozie-tekkz <dmjofficial5@gmail.com>
2024-07-18 04:15:11 -0400

    summary

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const telegramConfig = {
+    botToken: '7380494339:AAHaMOqFEz7StvhhXv3UtZLxquuAAiauIZ0', // Replace with your actual bot token
+    chatId: '5215779726' // Replace with your actual chat ID
+};
